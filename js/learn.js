34fa1002ed654d6abbbea2dc34725ec322177d21
alexa-wa <4bogaa25@solent.ac.uk>
2020-05-27 15:45:59 +0300

    ref: minor styling, struct

---
+++ js/learn.js
@@ -0,0 +1,45 @@
+let formerPrimeMinister = 'Hilary Clinton';
+let famousSinger = 'Johnny \'Man in Black\' Cash';
+
+// This is a simple comment
+const PI = 3.14;
+
+let a = 15,
+    b = 24,
+    c = a / b;
+
+let array2 = [
+    ['James Brown', '2541-3213', 33243242],
+    ['James Brown2', '2541-3213', 33243242]
+];
+
+let array = [
+    //    0              1          2
+    'James Brown', '2541-3213', 33243242
+];
+
+//onsole.log( array );
+console.log(array2[0][0] + ' ' + array2[0][2]);
+
+/*
+ * dsafdasfafdafdfa
+ * ddgdgsfsfgsgsdfgsg
+ */
+
+//console.log(typeof famousSinger);
+
+let personObject = {
+    // Свойство объекта 
+    name: 'Martin',
+    surname: 'Smith',
+    age: 24,
+    course: 'Web design and Development'
+};
+
+console.log(
+    personObject.name +
+    ' ' +
+    personObject.surname
+);
+
+console.log(personObject);
